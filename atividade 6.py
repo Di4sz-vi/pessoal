d1adfa759ae1a1a25cd874cb32e0ba3357c614a1
Di4sz-vi <vinicius_p_dias@estudante.sesisenai.org.br>
2025-04-25 10:19:25 -0300

    update

---
+++ atividade 6.py
@@ -0,0 +1,5 @@
+latas = int(input("Latas (350ml): "))
+g600 = int(input("Garrafas 600ml: "))
+g2l = int(input("Garrafas 2L: "))
+litros = (latas * 350 + g600 * 600 + g2l * 2000) / 1000
+print(f"Total: {litros} litros")
---
+++ att 25.py
@@ -0,0 +1,4 @@
+cateto1 = float(input("Cateto 1: "))
+cateto2 = float(input("Cateto 2: "))
+hipotenusa = math.sqrt(cateto1**2 + cateto2**2)
+print(f"Hipotenusa: {hipotenusa}")
